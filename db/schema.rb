78abec1d788211e430ab859b105d4491a5cec129
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-02-16 18:01:35 -0500

    getting comfortable with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209222541) do
+ActiveRecord::Schema.define(version: 20170209222137) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "first_name", limit: 25
